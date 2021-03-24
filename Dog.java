5ac72c14d3341511f40513275a494e3b0450d584
Angelica <Angelica137@users.noreply.github.com>
2021-03-24 21:03:02 +0000

    create Dog subclass

---
+++ Dog.java
@@ -0,0 +1,17 @@
+// extends Animal with extra attribute
+// exerciseTime(int)
+// getter and setters for exercise time
+// Override .toString to display dog's details
+
+public class Dog extends Animal {
+
+	// instance variables
+	private int exerciseTime;
+
+	// constructor
+	public Dog(String name, int time) {
+		super(name);
+		this.exerciseTime = exerciseTime;
+	}
+
+}
--- TestAnimal.java
+++ TestAnimal.java
@@ -3,13 +3,16 @@ public class TestAnimal {
 	public static void main(String args[]) {
 		// instantite an Animal object
 		Animal a = new Animal("CheChe");
-		System.out.println(a);
 
 		// .getName() returns name
 		System.out.println(a.getName());
 
 		// .toString() returns class details
 		System.out.println(a.toString());
+
+		// instntiate a Dog
+		Dog d = new Dog("Bobi", 30);
+		System.out.println(d);
 
 	}
 }
